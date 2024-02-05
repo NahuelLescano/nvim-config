d836f914e21f22be302dc125c11231d994b88b76
nahuel <lescanoagustin10nahuel@gmail.com>
2024-02-05 17:15:24 -0300

    Adding nice transparency to nvim

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,14 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("catppuccin-mocha")
-end,
+        function ColorMyPencils(color)
+            color = color or "catppuccin-mocha"
+            vim.cmd.colorscheme(color)
+
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        end
+
+        ColorMyPencils()
+    end,
 }
