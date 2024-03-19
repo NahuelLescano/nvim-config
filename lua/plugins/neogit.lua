7ed1e1ba27b829d99c1bb176abccfef8fb8139fd
Nahuel <lescanoagustin10nahuel@gmail.com>
2024-03-18 23:05:50 -0300

    Adding mappins

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,12 +4,21 @@ return {
         "nvim-lua/plenary.nvim",         -- required
         "sindrets/diffview.nvim",        -- optional - Diff integration
     },
+
     config = function ()
         require("neogit").setup()
         local neogit = require("neogit")
         vim.keymap.set("n", "<leader>gs", function()
             neogit.open({ kind = "split" })
         end)
-    end
 
+        vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>",
+            { silent = true, noremap = true })
+
+        vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>",
+            { silent = true, noremap = true })
+
+        vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>",
+            { silent = true, noremap = true })
+    end
 }
