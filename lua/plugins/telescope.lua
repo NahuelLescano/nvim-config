c3f9b2096a9c2ccdd557b78bec0ec7a5cd10b08d
Nahuel <lescanoagustin10nahuel@gmail.com>
2024-12-09 21:14:37 -0300

    Playing with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,57 +1,67 @@
 return {
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-        end, }, {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-                cond = function()
-                    return vim.fn.executable("make") == 1
-                end,
-            },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
         },
-
-        config = function()
-            pcall(require("telescope").load_extension, "fzf")
-            pcall(require("telescope").load_extension, "ui-select")
-
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
-
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[ ] Find existing buffers" })
-            vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search git files" })
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-
-            vim.keymap.set("n", "<leader>fg", function()
-                builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            end, { desc = "[S]earch a word in the project" })
-
-            vim.keymap.set("n", "<leader>sw", function()
-                local word = vim.fn.expand("<cword>")
-                builtin.grep_string({ search = word })
-            end, { desc = "[S]earch the highlighted [W]ord in the project" })
-
-            vim.keymap.set("n", "<leader>sW", function()
-                local word = vim.fn.expand("<cWORD>")
-                builtin.grep_string({ search = word })
-            end, { desc = "[S]earch the whole highlighted [W]ord in the project" })
-
-            vim.keymap.set("n", "<leader>sn", function()
-                builtin.find_files({ cwd = vim.fn.stdpath("config") })
-            end, { desc = "[S]earch [N]eovim files" })
+      })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
         end,
+      },
     },
+
+    config = function()
+      pcall(require("telescope").load_extension, "fzf")
+      pcall(require("telescope").load_extension, "ui-select")
+
+      require("telescope").setup({
+        pickers = {
+          find_files = {
+            theme = "ivy",
+          },
+        },
+      })
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
+
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[ ] Find existing buffers" })
+      vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search git files" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+
+      vim.keymap.set("n", "<leader>fg", function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      end, { desc = "[S]earch a word in the project" })
+
+      vim.keymap.set("n", "<leader>sw", function()
+        local word = vim.fn.expand("<cword>")
+        builtin.grep_string({ search = word })
+      end, { desc = "[S]earch the highlighted [W]ord in the project" })
+
+      vim.keymap.set("n", "<leader>sW", function()
+        local word = vim.fn.expand("<cWORD>")
+        builtin.grep_string({ search = word })
+      end, { desc = "[S]earch the whole highlighted [W]ord in the project" })
+
+      vim.keymap.set("n", "<leader>sn", function()
+        builtin.find_files({ cwd = vim.fn.stdpath("config") })
+      end, { desc = "[S]earch [N]eovim files" })
+    end,
+  },
 }
